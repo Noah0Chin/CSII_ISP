40ab41bbe843256706d8e77bc1522e4835052da0
Noah chin <william.chin@student.allensid.org>
2021-03-19 21:47:48 -0500

    HUGE struggle with adding UIKit, took a decent amount of time trying to figure things out.

--- Package.swift
+++ Package.swift
@@ -4,20 +4,58 @@
 import PackageDescription
 
 let package = Package(
+  name: "CSII-ISP",
+  products: [
+//    .library(
+//      name: "SwiftSoup",
+//      targets: ["SwiftSoup"]),
+//    .library(
+ //     name: "uikit",
+//      targets: ["uikit"]),
+  ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+     .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "1.7.4"),
+  //    .package(url: "https://github.com/uikit/uikit.git", from:"1.2.2"),
+
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+      
+        .target(
+            name: "CSII-ISP",
+            dependencies: ["SwiftSoup",]),
+//        path: "CSII-ISP"),
+
+       
+      //  .target(
+     //     name: "uikit",
+    //      dependencies: ["uikit"],
+  //        path: "CSII-ISP/uikit"),
+        .testTarget(
+            name: "CSII-ISPTests",
+            dependencies: ["CSII-ISP"]),
+    ]
+)
+/*
+let packageB = Package(
     name: "CSII-ISP",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-      .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "1.7.4"),
+      .package(url: "https://github.com/uikit/uikit.git", from: "3.6.18"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CSII-ISP",
-            dependencies: ["SwiftSoup"]),
+            dependencies: ["UIKit"]),
         .testTarget(
             name: "CSII-ISPTests",
             dependencies: ["CSII-ISP"]),
     ]
 )
+*/
--- Sources/CSII-ISP/main.swift
+++ Sources/CSII-ISP/main.swift
@@ -1,6 +1,25 @@
 import SwiftSoup
+import Foundation
+import 
 
 
+guard let url = URL(string: "https://na.op.gg/summoner/userName=Puffont") else {
+    fatalError("Failed to create URL")
+}
+
+//openURL(https://na.op.gg/summoner/userName=Puffont)
+
+//let link = openURL
+
+
+print("hello")
+
+
+
+
+
+
+/*
 func doIt() throws -> String {
     let text: String
     let html = "<html><head><title>First parse</title></head>"
@@ -12,3 +31,4 @@ func doIt() throws -> String {
 
 print(try doIt())
 
+*/
